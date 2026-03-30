cb32af908cb2943c603e6f6c3b7e266a0cd61e54
unknown <derekcha213@gmail.com>
2026-03-29 20:10:09 -0700

    renamed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DocuSmart",
+  title: "AIDocuReader",
   description: "An AI-powered document reader and question-answering tool.",
 };
 
